13d3171d2e66ca097d964090ed7ea7515571e127
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-06-14 21:19:48 +0300

    Auth Screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,8 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0x0);
+// const kPrimaryColor = Color(0xFF0C9869);
+const kPrimaryColor = Color.fromARGB(255, 0, 134, 201);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
